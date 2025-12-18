d4ebb1165f221f636b0d1befa1fb47ae898c2c89
oghomwenizekor-sys <oghomwen.izekor@binghamuni.edu.ng>
2025-12-18 21:44:00 +0100

    Update universal_language_dictionary1.py

--- universal_language_dictionary1.py
+++ universal_language_dictionary1.py
@@ -1,5 +1,5 @@
 
-Languages = 'Tiv = "a"\nIgbo = "b"\nJenjo = "c"\n'               #  PLEASE PUT YOUR LANGUAGE DIRECTLY BESIDE THE "\n" (no spacing) in the order you see (i.e \nYoruba = "c") the fifth person should not include the "\n" after their language
+Languages = 'Tiv = "a"\nIgbo = "b"\nJenjo = "c"\nEfik = d              #  PLEASE PUT YOUR LANGUAGE DIRECTLY BESIDE THE "\n" (no spacing) in the order you see (i.e \nYoruba = "c") the fifth person should not include the "\n" after their language
 eng_words = 'come\ntake\nsee\ngo\neat\nhouse\nsleep\nwalk\nwork\nplay\nask\nfood\nwater\nplease\neyes\nmake\nname\nlike\nlove\ngood'
 print('Welcome to the universal dictionary you may choose from the following languages')
 print(Languages)
@@ -27,12 +27,26 @@ if language_choice == 'c':
     print('you have chosen "Jenjo" language, you may now choose from the following words to translate:') #this is for Pheeyamila
 #---> Print your list of english words on this line (you can delete this)
 #then yea you can paste your code here->
-
+print(Alero's Addition)
 if language_choice == 'd':
-    print('you have chosen "Your_language" language, you may now choose from the following words to translate:')  # this is for Uriel, please replace 'Your_language' with your language for the dictionary
-    # ---> Print your list of english words on this line (you can delete this)
-    # then yea you can paste your code here->
-
+    print('you have chosen "Efik" language, you may now choose from the following words to translate:')  
+    print(eng_words)
+        Efik_dict{"hello": "mfo", "goodbye": "ka do", "thank you": "sosongo", "yes": "iyo", "no": "mba", "water": "mmong",
+    "food": "udon",  "house": "ufok","man": "eka", "woman": "edi", "child": "eyen",  "friend": "eka inam", "love": "ima",
+    "school": "ufok mme",  "book": "mbuk", "sun": "utong", "moon": "onen", "money": "owo", "work": "utomo", "name": "enyin"}
+print("Choose a language:")
+for key, value in languages.items():
+    print(key, "-", value[0])
+choice = input("Enter the number of the language: ")
+if choice in languages:
+    language_name, dictionary = languages[choice]
+    word = input("Enter an English word to translate: ").lower()
+  if word in dictionary:
+        print(f"The word in {language_name} is: {dictionary[word]}")
+    else:
+        print("Sorry, that word is not in the dictionary.")
+else:
+    print("Invalid language choice.")
 if language_choice == 'e':
-    print('you have chosen "Your_language" language, you may now choose from the following words to translate:')  # this is for Alero, please replace 'Your_language' with your language for the dictionary
+    print('you have chosen "Your_language" language, you may now choose from the following words to translate:')  # this is for Uriel, please replace 'Your_language' with your language for the dictionary
     # ---> Print your list of english words on this line (you can delete this)
